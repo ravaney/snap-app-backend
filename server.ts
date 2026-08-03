ac60cb1809008531392da9f5e1328e0cdedc847f
Your Name <lamar.lewis@lc.cuny.edu>
2026-08-02 23:24:41 -0400

    pkg json

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN ?? "http://localhost:5173",
     credentials: true,
   }),
 );
